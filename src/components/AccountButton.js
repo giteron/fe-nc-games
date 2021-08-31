7ae1413c11ae9be3e771084d0553e4655b5eb355
giteron <85683957+giteron@users.noreply.github.com>
2021-08-31 20:29:53 +0100

    implement basic profile button styling

--- src/components/AccountButton.js
+++ src/components/AccountButton.js
@@ -1,7 +1,10 @@
 const AccountButton = () => {
     return (
         <div className="AccountButton-container">
-            <p>Acc</p>
+            <section className="userDetails">
+                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="the user" />
+                <p>Acc</p>
+           </section>
         </div>
     );
 };
