0b2ef711164ac6d92f8a4e90ce9e74b6728b765d
giteron <85683957+giteron@users.noreply.github.com>
2021-09-01 11:24:17 +0100

    update import links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import AccountButton from './components/AccountButton';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import NavBar from './components/NavBar';
+import AccountButton from './components/AccountButton.jsx';
+import Header from './components/Header.jsx';
+import MainContent from './components/MainContent.jsx';
+import NavBar from './components/NavBar.jsx';
 
 
 function App() {
